ee1cb36cf2544a965d2ccc2da106cf5e0cb66d96
Amaranthusss <oskar.szkurlat@gmail.com>
2021-03-17 12:09:53 +0100

    Motors' test at new motherboard

--- arduino.js
+++ arduino.js
@@ -1,15 +1,20 @@
 //************Arduino Board & Port Configuration************
-var five = require('johnny-five');
-board = new five.Board({ port: "/dev/ttyACM0" });
+var {Board, Led} = require('johnny-five')
+board = new Board({ port: "/dev/ttyAMA0" })
 const config = require('./config/config')
+const pinout = require('./config/pinout')
 var arStatus = 'closed'
 
 //************Objects' List************
 board.on("ready", function () {
     arStatus = 'ready'
     console.log("[ARDUNIO] Here we go!")
-    const scanners = require('./scanners/scanners')
-    setTimeout(() => { scanners.rearScanner.start(); console.log('Scanner has been started') }, config.SCANNERS.DELAY_TO_START)
+    //const scanners = require('./scanners/scanners')
+    //setTimeout(() => { scanners.rearScanner.start(); console.log('Scanner has been started') }, config.SCANNERS.DELAY_TO_START)
+
+
+    const errorDiode = new Led(pinout.errorDiode.state)
+    errorDiode.blink(500)
 
 
     board.on("exit", () => {
--- main.js
+++ main.js
@@ -5,6 +5,7 @@
 //___________ Modules ___________
 const arduino = require('./arduino')
 const server = require('./server/server')
+const axises = require('./config/pinout').axises
 //___________ Async Server ___________
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8081 })
@@ -29,5 +30,34 @@ wss.on('connection', function connection(ws) {
         }
     }, 10000)
 })
-
 //setInterval(() => { console.log('data', scannerCtrl.data) }, 1000)
+
+const raspGpio = require('pigpio').Gpio
+const en = new raspGpio(axises.leftFront.motor.EN, { mode: raspGpio.OUTPUT })
+const dir = new raspGpio(axises.leftFront.motor.DIR, { mode: raspGpio.OUTPUT })
+const step = new raspGpio(axises.leftFront.motor.STEP, { mode: raspGpio.OUTPUT, pullUpDown: raspGpio.PUD_UP })
+
+en.digitalWrite(0)
+dir.digitalWrite(0)
+let freq = 700 //8k max
+let pwm = 250
+let dirB = false
+step.pwmWrite(pwm)
+step.pwmFrequency(freq)
+setInterval(() => {
+    if (dirB == false)
+        freq = freq + 100
+    else
+        freq = freq - 100
+    if (freq > 2500) {
+        dirB = true
+        freq = 2400
+    } else if (freq < 500) {
+        dirB = false
+        freq = 600
+    }
+
+    step.pwmFrequency(freq)
+    console.log(pwm, step.getPwmFrequency(), freq)
+}, 1000)
+//console.log(step.getPwmFrequency())
