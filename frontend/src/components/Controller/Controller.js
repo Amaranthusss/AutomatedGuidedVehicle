eef74082e040d6c131639327150c1c843e44029a
Amaranthusss <oskar.szkurlat@gmail.com>
2021-04-02 17:50:34 +0200

    Keyboard handler at frontend to control agv movements

--- frontend/src/components/Controller/Controller.js
+++ frontend/src/components/Controller/Controller.js
@@ -7,39 +7,58 @@ import axios from 'axios'
 class Controller extends React.Component {
     constructor(props) {
         super(props)
+        this.states = [
+            { state: false, ref: '/upCmd', lastType: '' },
+            { state: false, ref: '/downCmd', lastType: '' },
+            { state: false, ref: '/leftCmd', lastType: '' },
+            { state: false, ref: '/rightCmd', lastType: '' }
+        ]
+        document.addEventListener("keydown", this._keyboardHandler) //Detection at all web page
+        document.addEventListener("keyup", this._keyboardHandler) //Detection at all web page
     }
-    forwardEvent = async e => {
-        let state
-        if (e.type === "mousedown") state = true
-        else state = false
-        await axios.post('/forwardCmd', { state: state })
+    _keyboardHandler = async e => { //Key events have intervals so there was required transfer limit
+        let id, object
+        switch (e.key) { //One handler to all specified keys
+            case 'w': id = 0; break
+            case 's': id = 1; break
+            case 'a': id = 2; break
+            case 'd': id = 3; break
+            default: return; break
+        }
+        object = this.states[id] //Pick suitable state
+        if (e.type === 'keydown')
+            object.state = true
+        else object.state = false
+        if (object.lastType != e.type) //Command has to be different than previous
+            axios.post(object.ref, { state: object.state })
+        object.lastType = e.type //Backup current state
     }
-    backwardEvent = async e => {
-        let state
-        if (e.type === "mousedown") state = true
-        else state = false
-        await axios.post('/backwardCmd', { state: state })
-    }
-    leftEvent = async e => {
-        let state
-        if (e.type === "mousedown") state = true
-        else state = false
-        await axios.post('/leftCmd', { state: state })
-    }
-    rightEvent = async e => {
-        let state
-        if (e.type === "mousedown") state = true
-        else state = false
-        await axios.post('/rightCmd', { state: state })
+    _buttonHandler = async e => {
+        let id, object
+        switch (e.target.id) { //One handler to all controller's buttons
+            case 'upBttn': id = 0; break
+            case 'downBttn': id = 1; break
+            case 'leftBttn': id = 2; break
+            case 'rightBttn': id = 3; break
+        }
+        object = this.states[id] //Pick suitable state
+        if (e.type === 'mousedown')
+            object.state = true
+        else object.state = false
+        await axios.post(object.ref, { state: object.state })
     }
 
     render() {
         return (
             <div>
-                <Button onMouseDown={this.forwardEvent} onMouseUp={this.forwardEvent}>Forward</Button><br></br>
-                <Button onMouseDown={this.leftEvent} onMouseUp={this.leftEvent}>Left</Button>
-                <Button onMouseDown={this.rightEvent} onMouseUp={this.rightEvent}>Right</Button><br></br>
-                <Button onMouseDown={this.backwardEvent} onMouseUp={this.backwardEvent}>Backward</Button>
+                <Button id='upBttn' onKeyDownCapture={this._keyboardHandler} onKeyUpCapture={this._keyboardHandler}
+                    onMouseDown={this._buttonHandler} onMouseUp={this._buttonHandler}>Forward</Button><br></br>
+                <Button id='leftBttn' onKeyDownCapture={this._keyboardHandler} onKeyUpCapture={this._keyboardHandler}
+                    onMouseDown={this._buttonHandler} onMouseUp={this._buttonHandler}>Left</Button>
+                <Button id='rightBttn' onKeyDownCapture={this._keyboardHandler} onKeyUpCapture={this._keyboardHandler}
+                    onMouseDown={this._buttonHandler} onMouseUp={this._buttonHandler}>Right</Button><br></br>
+                <Button id='downBttn' onKeyDownCapture={this._keyboardHandler} onKeyUpCapture={this._keyboardHandler}
+                    onMouseDown={this._buttonHandler} onMouseUp={this._buttonHandler}>Backward</Button>
             </div >
         )
     }
