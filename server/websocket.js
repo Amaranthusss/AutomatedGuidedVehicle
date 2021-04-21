58882c6933a2ea826fe373de52535af41bb68c05
Amaranthusss <oskar.szkurlat@gmail.com>
2021-04-21 12:59:16 +0200

    Backed to older version

--- server/websocket.js
+++ server/websocket.js
@@ -1,24 +1,18 @@
 
+const WebSocket = require('wss')
 const port = require('../config/config').SERVER.ASYNC_PORT
-const { createServer } = require('wss')
-const wss = createServer(function connectionListener(ws) {
-  wss = ws
-  ws.send('welcome!')
-  ws.on('message', (data) => {
-    ws.send(data.toString()) // echo-server
-  })
-})
-  .listen(port, function () {
-    const { address, port } = this.address() // this is the http[s].Server
-    console.log('listening on http://%s:%d (%s)', /::/.test(address) ? '0.0.0.0' : address, port)
-  })
+const wss = WebSocket.createServer()
+const httpsCreateServer = require('https').createServer
+const https = httpsCreateServer()
+https.listen(port)
 
-// wsServer.on('connection', function connection(ws) {
-//   console.log('[WebSocket]', 'Server has been established')
-//   // ws.on('message', function incoming(message) {
-//   //   let packet = JSON.parse(message)
-//   //   if (packet.cmd === 'forward')
-//   //     console.log(packet.data)
-//   // })
-// })
+/*
+wss.on('connection', function connection(ws) {
+  console.log('[WebSocket]', 'Server has been established')
+  ws.on('message', function incoming(message) {
+    let packet = JSON.parse(message)
+    if (packet.cmd === 'forward')
+      console.log(packet.data)
+  })
+})*/
 module.exports = wss
